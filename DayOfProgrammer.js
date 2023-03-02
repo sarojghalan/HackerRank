ec2b5769e628d38b08821dfa821b79ba0d4325ca
Hypekage <jmestmnag@gmail.com>
2023-03-02 15:08:32 +0545

    day has been calculated only formatting remaining

--- DayOfProgrammer.js
+++ DayOfProgrammer.js
@@ -1,4 +1,12 @@
-function DayOfProgrammer(){
+function DayOfProgrammer(year, day) {
+    let daySum = 0;
+  function getDaysInMonth(year, month) {
+    return new Date(year, month, 0).getDate();
+  }
+  for(let i = 1;i<=8;i++){
+    daySum += getDaysInMonth(year,i); 
+  }
+  return new Date(year,09,day-daySum).toLocaleDateString()
 
 }
-DayOfProgrammer(2010);
+DayOfProgrammer(1984, 256);
