1f6ccc9f369429711bface327bbe2d6972c96f80
Hypekage <jmestmnag@gmail.com>
2023-05-22 23:55:32 +0545

    front page count calculated

--- DrawingBooks.js
+++ DrawingBooks.js
@@ -1,6 +1,19 @@
 function DrawingBooks(totalPage, actualPage) {
-    console.log(totalPage,actualPage);
+  console.log(totalPage, actualPage);
+  let frontPageCount = 0;
+  let backPageCount = 0;
+  for (let i = 1; i < totalPage; i++) {
+    if (i === actualPage) {
+      let frontValue = i;
+      let pageChecker = !Number.isInteger((frontValue - 1) / 2);
+      console.log(pageChecker);
+      frontPageCount = pageChecker
+        ? (frontValue - 1) / 2
+        : (frontValue) / 2;
+    }
+  }
+  console.log(frontPageCount);
   return;
 }
 
-const Test = DrawingBooks(12, 5);
+const Test = DrawingBooks(11, 8);
