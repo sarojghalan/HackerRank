ae927faaf941473e0cf310b4c7502bbe7aba01e9
Hypekage <jmestmnag@gmail.com>
2023-05-23 21:02:11 +0545

    Counting Valley challenge accepted

--- CountingValley.js
+++ CountingValley.js
@@ -3,7 +3,24 @@ function countingValleys(steps, path) {
   let valleyCount = 0; // number of valleys traversed
   let isInValley = false; // flag to track if the hiker is in a valley
 
+  for (let i = 0; i < steps; i++) {
+    if (path[i] === "U") {
+      altitude++; // step up
+    } else if (path[i] === "D") {
+      altitude--; // step down
 
+      // check if the hiker entered a valley
+      if (altitude < 0 && !isInValley) {
+        isInValley = true;
+      }
+    }
+
+    // check if the hiker left the valley
+    if (altitude === 0 && isInValley) {
+      valleyCount++;
+      isInValley = false;
+    }
+  }
 
   return valleyCount;
 }
