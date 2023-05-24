1b64a6bd80785a439cd800ab93c7203c64bcef3e
Hypekage <jmestmnag@gmail.com>
2023-05-24 20:24:48 +0545

    Electronics Shop challenge completed

--- ElectronicShops.js
+++ ElectronicShops.js
@@ -1,3 +1,19 @@
 function ElectronicShop(keyboards, drives, b) {
+  let totalPriceArray = [];
+  // calculating the sum price of both through iteration of arr1
+  for (let i = 0; i < keyboards.length; i++) {
+    // getting the number below budget for each one keyboard and usb
+    let totalArray = drives
+      .map((item) => item + keyboards[i])
+      .filter((item) => item <= b);
+    totalPriceArray = totalPriceArray.concat(totalArray);
+  }
+  let maxBudget =
+    totalPriceArray.length === 0 ? -1 : Math.max(...totalPriceArray);
+  console.log(maxBudget);
   return;
 }
+const arr1 = [3, 1];
+const arr2 = [5, 2, 8];
+const budget = 10;
+const Shop = ElectronicShop(arr1, arr2, budget);
