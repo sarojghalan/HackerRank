98ebe91a9511ec9a75d7dbb7ed68df39cd258dc7
Hypekage <jmestmnag@gmail.com>
2023-05-24 23:18:42 +0545

    Cat and Mouse Challlemge logic cracked

--- CatAndMouse.js
+++ CatAndMouse.js
@@ -1,8 +1,29 @@
-function CatAndMouse(x,y,z){
-    console.log(x,y,z);
-    return;
+function CatAndMouse(x, y, z) {
+  let catUnitA = z - x;
+  let catUnitB = z - y;
+  let returnUnit = "";
+
+  if (catUnitA < 0 && catUnitB < 0) {
+    if (catUnitA > catUnitB) {
+      returnUnit = "Cat A";
+    } else if (catUnitA < catUnitA) {
+      returnUnit = "Cat B";
+    } else {
+      returnUnit = "Mouse C";
+    }
+  } else {
+    if (Math.abs(catUnitA) > Math.abs(catUnitB)) {
+      returnUnit = "Cat B";
+    } else if (Math.abs(catUnitA) < Math.abs(catUnitB)) {
+      returnUnit = "Cat A";
+    } else {
+      returnUnit = "Mouse C";
+    }
+  }
+  return returnUnit;
 }
 const catA = 1;
-const catB = 2;
-const mouseC = 3;
-const Catch = CatAndMouse(catA,catB,mouseC);
+const catB = 3;
+const mouseC = 2;
+const Catch = CatAndMouse(catA, catB, mouseC);
+console.log(Catch);
