95d63c74a2aea49e88db612adb66d9c4a1d3b8cf
Hypekage <jmestmnag@gmail.com>
2023-05-26 11:51:43 +0545

    forming square logic cracked

--- MagicSquare.js
+++ MagicSquare.js
@@ -1,6 +1,59 @@
 function MagicSquare(a) {
-  console.log("MagicSquare", a);
-  return;
+  // Possible cases for all 2D array
+  const possibleCases = [
+    [
+      [8, 1, 6],
+      [3, 5, 7],
+      [4, 9, 2],
+    ],
+    [
+      [4, 3, 8],
+      [9, 5, 1],
+      [2, 7, 6],
+    ],
+    [
+      [2, 9, 4],
+      [7, 5, 3],
+      [6, 1, 8],
+    ],
+    [
+      [6, 7, 2],
+      [1, 5, 9],
+      [8, 3, 4],
+    ],
+    [
+      [6, 1, 8],
+      [7, 5, 3],
+      [2, 9, 4],
+    ],
+    [
+      [8, 3, 4],
+      [1, 5, 9],
+      [6, 7, 2],
+    ],
+    [
+      [4, 9, 2],
+      [3, 5, 7],
+      [8, 1, 6],
+    ],
+    [
+      [2, 7, 6],
+      [9, 5, 1],
+      [4, 3, 8],
+    ],
+  ];
+
+  let min = 45;
+  possibleCases.forEach((square) => {
+    let sum = 0;
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        sum += Math.abs(s[i][j] - square[i][j]);
+      }
+    }
+    if (sum < min) min = sum;
+  });
+  return min;
 }
 const arr = [
   [5, 3, 4],
