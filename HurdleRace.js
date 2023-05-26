8d418240554ff4c2b72c912c8f061f46261adc66
Hypekage <jmestmnag@gmail.com>
2023-05-26 13:13:11 +0545

    comment added to HurdleRace

--- HurdleRace.js
+++ HurdleRace.js
@@ -1,6 +1,10 @@
 function HurdleRace(k, h) {
+    // filtering value greater than max height
   let highest = h.filter((i) => i > k);
+
+  // storing highest number from filter array
   let highValue = Math.max(...highest);
+  // rendering 0 if filter array has no greater value than max height
   let dose = highest.length === 0 ? 0 : highValue - k;
   return dose;
 }
