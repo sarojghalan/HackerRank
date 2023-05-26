3f91c55ac7dd56ef74a7afbad7fbd152827e2f87
Hypekage <jmestmnag@gmail.com>
2023-05-26 13:47:21 +0545

    Designer PDF Viewer challenge accepted

---
+++ DesignerPDFViewer.js
@@ -0,0 +1,9 @@
+function DesignerPDFViewer(h, word) {
+  console.log(h, word);
+  return;
+}
+const word = "abcz";
+const array = [
+  1, 3, 1, 3, 1, 4, 1, 3, 2, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5,
+];
+const designer = DesignerPDFViewer(array, word);
