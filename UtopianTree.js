4574bd54eb60d2aae95e947aa6571491ac35b917
Hypekage <jmestmnag@gmail.com>
2023-05-27 21:10:39 +0545

    utopian tree challenge refined and add comment

--- UtopianTree.js
+++ UtopianTree.js
@@ -1,6 +1,8 @@
 function utopianTree(n) {
   let height = 1; // at least height === 1, (if n === 0);
   for (let i = 1; i <= n; i++) {
+    // if current cycle is odd , height will be doubled
+    // if current cycle is even, height will be increases by 1
     i % 2 !== 0 ? (height *= 2) : (height += 1);
   }
   return height;
