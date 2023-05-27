9d79252bd9dd244f7a6f535667319313c5fea6b9
Hypekage <jmestmnag@gmail.com>
2023-05-27 20:38:22 +0545

    Designer PDF Viewer challenge logic cracked

--- DesignerPDFViewer.js
+++ DesignerPDFViewer.js
@@ -1,8 +1,23 @@
+function getAllAlphabets() {
+  var alphabets = "";
+  for (var j = 97; j <= 122; j++) {
+    alphabets += String.fromCharCode(j); // Lowercase letters (a-z)
+  }
+  return alphabets;
+}
+
 function DesignerPDFViewer(h, word) {
-  console.log(h, word);
+  let temp = [];
+  for (let i = 0; i < h.length; i++) {
+    if (word.includes(getAllAlphabets()[i])) {
+      temp.push(h[i]);
+    }
+  }
+  let rectangleArea = Math.max(...temp) * word.length;
+  console.log(rectangleArea);
   return;
 }
-const word = "abcz";
+const word = "zbca";
 const array = [
   1, 3, 1, 3, 1, 4, 1, 3, 2, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5,
 ];
