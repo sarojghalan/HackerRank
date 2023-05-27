dd0b21ff26aeda80236c8d46f2eaac03acaa39de
Hypekage <jmestmnag@gmail.com>
2023-05-27 21:09:05 +0545

    Utopian tree logic cracked

--- UtopianTree.js
+++ UtopianTree.js
@@ -1,6 +1,9 @@
-function UtopianTree(n){
-    console.log(arr);
-    return;
+function utopianTree(n) {
+  let height = 1; // at least height === 1, (if n === 0);
+  for (let i = 1; i <= n; i++) {
+    i % 2 !== 0 ? (height *= 2) : (height += 1);
+  }
+  return height;
 }
-const arr = [2,0,1];
-const tree = UtopianTree(arr);
+const arr = [2, 0, 1];
+const tree = utopianTree(arr);
