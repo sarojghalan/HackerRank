53f5816afa235f5b5e5d91cf3e9089295529402a
Hypekage <jmestmnag@gmail.com>
2023-05-28 17:48:31 +0545

    beautiful days challenge accepted

---
+++ BeautifulDays.js
@@ -0,0 +1,9 @@
+function BeautifulDays(i, j, k) {
+  console.log(i, j, k);
+  return;
+}
+
+const rangeStart = 20;
+const rangeEnd = 23;
+const day = 6;
+const result = BeautifulDays(rangeStart, rangeEnd, day);
