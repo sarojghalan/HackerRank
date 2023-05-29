c7c7221ae256dceee3f9f8d461971149e038ff52
Hypekage <jmestmnag@gmail.com>
2023-05-29 12:41:56 +0545

    viral advertising logic cracked

--- ViralAdvertising.js
+++ ViralAdvertising.js
@@ -1,6 +1,20 @@
 function ViralAdvertising(n) {
-  console.log(n);
-  return;
+  let totalDays = 5;
+  let count = 0;
+  let viewer = 0;
+  for (let i = 1; i <= n; i++) {
+    if (i === 1) {
+      count = Math.floor(totalDays / 2);
+    } else {
+      console.log("total days ", totalDays);
+      viewer = Math.floor(totalDays / 2) * 3;
+      totalDays = viewer;
+      console.log("viewer", viewer);
+      count = count + Math.floor(totalDays / 2);
+      console.log("count", count);
+    }
+  }
+  return count;
 }
-const day = 3;
+const day = 5;
 const result = ViralAdvertising(day);
