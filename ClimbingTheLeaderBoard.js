8a2cf9f783ba201b24b80f6076c956da221c7730
Hypekage <jmestmnag@gmail.com>
2023-05-29 14:27:12 +0545

    Climbing the leaderboard challenge accepted

---
+++ ClimbingTheLeaderBoard.js
@@ -0,0 +1,8 @@
+function ClimbingLeaderBoard(ranked, players) {
+  console.log(ranked, players);
+  return;
+}
+
+const ranked = [100, 90, 90, 80];
+const players = [70, 80, 90];
+const result = ClimbingLeaderBoard(ranked, players);
