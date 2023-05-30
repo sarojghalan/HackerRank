9af589703b9e663b704d9e1eb4a753a25331dee2
Hypekage <jmestmnag@gmail.com>
2023-05-30 23:17:32 +0545

    save the prisoner problem solved

--- SavePrisoner.js
+++ SavePrisoner.js
@@ -1,8 +1,21 @@
-function savePrisoner(n, m, s) {
-  console.log(n, m, s);
-  return;
+function saveThePrisoner(n, m, s) {
+  // Calculating the remainder (position of the last treat relative to the starting chair)
+  const remainder = m % n;
+  console.log(remainder)
+
+  // Calculate the chair number of the prisoner to warn
+  let warnChair = (s + remainder - 1) % n;
+  console.log(warnChair);
+
+  // Handle the case where warnChair is 0 (last chair)
+  // if (warnChair === 0) {
+  //   warnChair = n;
+  // }
+
+  return warnChair;
 }
-const total = 5;
-const noOfSweet = 2;
-const start = 2;
-const result = savePrisoner(total, noOfSweet, start);
+const n = 3; // number of prisoners
+const m = 7; // number of treats
+const s = 3; // starting chair
+
+const prisonerToWarn = saveThePrisoner(n, m, s);
