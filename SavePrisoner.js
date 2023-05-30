ac8489481b808ed8f5283df14e213d7b8a5ee8fa
Hypekage <jmestmnag@gmail.com>
2023-05-30 20:01:34 +0545

    Save the prisoner challenge accepted

---
+++ SavePrisoner.js
@@ -0,0 +1,8 @@
+function savePrisoner(n, m, s) {
+  console.log(n, m, s);
+  return;
+}
+const total = 5;
+const noOfSweet = 2;
+const start = 2;
+const result = savePrisoner(total, noOfSweet, start);
