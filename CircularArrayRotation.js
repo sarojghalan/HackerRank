30ae3cf783dcc5e7f046e4c1a0c9f11440fe4c96
Hypekage <jmestmnag@gmail.com>
2023-05-31 23:54:52 +0545

    Circular Array Rotating logic cracked

--- CircularArrayRotation.js
+++ CircularArrayRotation.js
@@ -1,8 +1,20 @@
-function CircularArrayRotation(n, k, queries) {
-  console.log(queries, n, k);
-  return;
+function CircularArrayRotation(a, k, queries) {
+  let updateArray = a;
+  let rotatedArray = [];
+  // process for last out and first in
+  for (let i = 0; i < k; i++) {
+    let lastElement = updateArray.pop();
+    updateArray.unshift(lastElement);
+  }
+  let maxElem = queries.pop();
+  for (let i = queries[0]; i <= maxElem; i++) {
+    rotatedArray.push(updateArray[i]);
+  }
+  console.log(rotatedArray);
+  return rotatedArray;
 }
 
-const array = [1, 2, 3];
+const array = [4, 5, 6];
 const totalNumber = 2;
 const queries = [0, 1, 2];
+const result = CircularArrayRotation(array, totalNumber, queries);
