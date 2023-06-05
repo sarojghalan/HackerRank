2acad1b44ecb400a7238ad3eb73d8c15829a99c1
Hypekage <jmestmnag@gmail.com>
2023-06-05 22:31:28 +0545

    find the digit solved

--- FIndTheDigit.js
+++ FIndTheDigit.js
@@ -1,6 +1,14 @@
 function FindTheDigit(n) {
-    console.log(n);
-    return;
+    let number = n.toString();
+    let count = 0;
+  console.log(n.toString());
+  for (let i = 0; i < number.length; i++) {
+        if(n%number[i] === 0){
+            count++
+        }
+  }
+  console.log(count)
+  return count;
 }
 const digit = 123;
-const result = FindTheDigit(digit)
+const result = FindTheDigit(digit);
